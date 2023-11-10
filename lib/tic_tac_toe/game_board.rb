b460075b2bc9ec63baef4c5181ea0d7f2f307dfe
Hristo Kochev <h.l.kochev@gmail.com>
2023-11-10 18:48:23 +0200

    rake-nix added

--- lib/tic_tac_toe/game_board.rb
+++ lib/tic_tac_toe/game_board.rb
@@ -1,24 +1,43 @@
-module TicTacToe
-  
-  Position = Data.define(:row, :col) 
-  Move = Data.define(:player, :position)
+# frozen_string_literal: true
 
+module TicTacToe
+  # classes to work with game board
+  Position = Data.define(:row, :col)
+  Move = Data.define(:player, :position)
+  ##
+  # Game board logic
   class GameBoard
     attr_reader :moves
 
-    def self.default_players ["X", "O"] end 
-    
-    def initialize(players: self.default_player)
+    def self.default_players
+      %w[X O]
+    end
+
+    def initialize(players: GameBoard.default_players)
       @first = true
       @players = players
       @moves = []
     end
-    
-    def nextMove(:position)
-       
+
+    def next_move(position:)
+      @moves << Move.new(next_player, position)
+      self
     end
 
-    def prev_move()
-    end  
-  end  
-end  
+    def prev_move
+      @moves.pop
+      self
+    end
+
+    private
+
+    def next_player
+      if @first
+        @players[0]
+      else
+        @players[1]
+      end
+      @first = !@first
+    end
+  end
+end
---
+++ spec/tic_tac_toe/game_board_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/tic_tac_toe/game_board'
+require 'pry-debugger'
+
+RSpec.describe TicTacToe::GameBoard do
+  let(:game_board) { TicTacToe::GameBoard.new }
+
+  describe '#initialize' do
+    it 'creates a GameBoard with default players' do
+      expect(game_board.moves).to be_empty
+      expect(game_board.instance_variable_get(:@players)).to eq(%w[X O])
+    end
+
+    it 'creates a GameBoard with custom players' do
+      custom_players = %w[A B]
+      custom_game_board = TicTacToe::GameBoard.new(players: custom_players)
+      expect(custom_game_board.instance_variable_get(:@players)).to eq(custom_players)
+    end
+  end
+
+  describe '#next_move' do
+    it 'adds a move to the board' do
+      position = TicTacToe::Position.new(1, 1)
+      game_board.next_move(position: position)
+      expect(game_board.moves).to contain_exactly(an_instance_of(TicTacToe::Move))
+    end
+
+    it 'alternates between players' do
+      position1 = TicTacToe::Position.new(1, 1)
+      position2 = TicTacToe::Position.new(2, 2)
+
+      binding.pry # puts game_board.moves
+      game_board.next_move(position: position1)
+      expect(game_board.moves.last.player).to eq('X')
+
+      game_board.next_move(position: position2)
+      expect(game_board.moves.last.player).to eq('O')
+    end
+  end
+
+  describe '#prev_move' do
+    it 'removes the last move from the board' do
+      position = TicTacToe::Position.new(1, 1)
+      game_board.next_move(position: position)
+      expect { game_board.prev_move }.to change { game_board.moves.length }.by(-1)
+    end
+  end
+end
